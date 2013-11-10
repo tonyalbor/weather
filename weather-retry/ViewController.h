ee8e4520199b5aea2e1d768ac89257c1caf85807
Gustavo Blanco <gblan002@ucr.edu>
2013-11-09 16:29:53 -0800

    fghj

--- weather-retry/ViewController.h
+++ weather-retry/ViewController.h
@@ -2,7 +2,7 @@
 //  ViewController.h
 //  weather-retry
 //
-//  Created by Tony Albor on 11/9/13.
+//  Created by Tony Albor & Gus on 11/9/13.
 //  Copyright (c) 2013 tonyalbor. All rights reserved.
 // a;slkdfjadsf
 
