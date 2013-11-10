0f475bff713a5b8df8d7fddd4ec780cb6958c09c
Gustavo Blanco <gblan002@ucr.edu>
2013-11-09 16:35:55 -0800

    works now

--- weather-retry/ViewController.h
+++ weather-retry/ViewController.h
@@ -4,9 +4,7 @@
 //
 //  Created by Tony Albor & Gus on 11/9/13.
 //  Copyright (c) 2013 tonyalbor. All rights reserved.
-// a;slkdfjadsf
-// as;dlfjas;dlfkjads
-// a;lsdjf;alsdjf
+
 
 #import <UIKit/UIKit.h>
 
