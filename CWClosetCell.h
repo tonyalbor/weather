876bbb2ca65fc760b8878f1c04201595abba50f8
Tony Albor <talbo001@ucr.edu>
2014-01-14 15:37:23 -0800

    created closet cell view

---
+++ CWClosetCell.h
@@ -0,0 +1,14 @@
+//
+//  CWClosetCell.h
+//  weather-retry
+//
+//  Created by Tony Albor on 1/14/14.
+//  Copyright (c) 2014 tonyalbor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CWClosetCell : UICollectionViewCell
+@property (weak, nonatomic) IBOutlet UILabel *nameLabel;
+
+@end
---
+++ CWClosetCell.m
@@ -0,0 +1,31 @@
+//
+//  CWClosetCell.m
+//  weather-retry
+//
+//  Created by Tony Albor on 1/14/14.
+//  Copyright (c) 2014 tonyalbor. All rights reserved.
+//
+
+#import "CWClosetCell.h"
+
+@implementation CWClosetCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
