9a5e9ef239ec49a81862148584be85a840b1016c
Tony Albor <talbo001@ucr.edu>
2014-01-20 21:20:33 -0800

    added files , initial commit

---
+++ weather-retry/CWViewController.h
@@ -0,0 +1,13 @@
+//
+//  CWViewController.h
+//  weather-retry
+//
+//  Created by Tony Albor on 1/20/14.
+//  Copyright (c) 2014 tonyalbor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CWViewController : UIViewController
+
+@end
---
+++ weather-retry/CWViewController.m
@@ -0,0 +1,38 @@
+//
+//  CWViewController.m
+//  weather-retry
+//
+//  Created by Tony Albor on 1/20/14.
+//  Copyright (c) 2014 tonyalbor. All rights reserved.
+//
+
+#import "CWViewController.h"
+
+@interface CWViewController ()
+
+@end
+
+@implementation CWViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
